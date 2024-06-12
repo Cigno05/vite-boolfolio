6cf6a02fd930ffb2af61065369f87e3946ffbdc5
Cigno05 <paolocalcagni.82@gmail.com>
2024-06-12 19:45:18 +0200

    Print result of api in page

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,11 @@ export default {
           }
       })
       .then(res => {
-        console.log(res.data.result.data)
+        // console.log(this.store.projects, 'progetti')
+        this.store.projects = res.data.result.data
+        // console.log(this.store.projects, 'progetti dopo')
+
+        
       })
     }
   },
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,15 @@
 <script>
+import { store } from '../store.js'
 
 export default {
   components: {
   },
+  created() {
+    // console.log(store.projects[0].technologies);
+  },
   data() {
     return {
+      store,
     }
   }
 }
@@ -14,7 +19,24 @@ export default {
 <template>
 
   <main>
-    <h1>Main</h1>
+    <div class="container">
+      <div class="row">
+
+        <div class="col-4 card-group" v-for="project in store.projects" :key="project.id">
+            <div class="card my-3">
+              <div class="card-body">
+                <h1 class="card-title mb-4">{{ project.title }}</h1>
+                <p class="card-text">Created: {{ project.creation_date }}</p>
+                <p class="card-text">Project Type: {{ project.type?.name }}</p>
+                <p class="card-text" >Technologies used: <span v-for="technology in project.technologies" :key="technology.id">{{ technology.name }} </span></p> <!-- mettere lo spazio -->
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+
+
+
   </main>
 
 </template>
